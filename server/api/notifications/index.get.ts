656fdc3e2cdae3c046633f652943c0139f892d41
Cody Wakeford <codypwakeford@gmail.com>
2024-10-15 10:17:28 +0000

    minor changes

--- server/api/notifications/index.get.ts
+++ server/api/notifications/index.get.ts
@@ -1,19 +1,22 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs } from "firebase/firestore"
 
 export default eventHandler(async (event): Promise<AppNotification[]> => {
-    const db = event.context.db;
+    const db = event.context.velorisDb
+    const { id }: { id: string } = await readBody(event)
 
-    const colRef = collection(db, "notifications");
+    const usersColRef = collection(db, "users")
+    const userDocRef = doc(usersColRef, id)
+    const userNotificationColRef = collection(userDocRef, "notifications")
 
-    const snapShot = await getDocs(colRef);
+    const snapShot = await getDocs(userNotificationColRef)
 
     const notifications = snapShot.docs.map(
         (doc) =>
             ({
                 id: doc.id,
                 ...doc.data(),
-            }) as AppNotification,
-    );
+            }) as AppNotification
+    )
 
-    return notifications;
-});
+    return notifications
+})
